7b7b7333bf509c601a097b4b55e479842da282e1
Ryan Hasler <rhasler004@gmail.com>
2023-06-01 12:17:50 -0400

    booleans solved

--- index.js
+++ index.js
@@ -15,3 +15,36 @@
 ** 50/50
 */
 
+/**********************************************************
+ * B. Strings
+ **********************************************************/
+
+let firstVariable = "Hello World";
+firstVariable = 2;
+let secondVariable = firstVariable;
+secondVariable = "Goodbye World";
+let yourName = "Ryan Hasler";
+console.log("Hello, my name is " + yourName);
+console.log(firstVariable);
+
+/*********************************************************
+ * C. Booleans
+ *********************************************************/
+
+const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' === 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true || false);
+  console.log(false || false || false || false || false || true);
+  console.log(false === false)
+  console.log(e === 'Kevin');
+  console.log(a < b + c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a != a + d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
