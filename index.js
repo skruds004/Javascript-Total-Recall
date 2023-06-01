e3b5fdcc8bec2ce548cff056b7fc625c8f259615
Ryan Hasler <rhasler004@gmail.com>
2023-06-01 12:11:31 -0400

    answered questions (hopefully correctly)

---
+++ index.js
@@ -0,0 +1,17 @@
+/******************* QUESTIONS **************************** 
+* 1. How do we assign a value to a variable?
+** We use the assignment operator (=)
+* 2. How do we change the value of a variable?
+** We also use the assignment operator (=) and can use +, -, /, *, etc. to perform operations on the variable
+* 3. How do we assign an existing variable to a new variable?
+** Using the assignment operator, EX) let newVar = existingVar;
+* 4. Remind me, what are declare, assign, and define?
+** Delcaration is registering the variable in its scope
+** Assignment is giving a value to the variable
+** Defining a variable is giving it a type
+* 5. What is pseudocoding and why should you do it?
+** Pseudocoding is planning out your code in informal language that is used to design your program before writing it
+* 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+** 50/50
+*/
+
